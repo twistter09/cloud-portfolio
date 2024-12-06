43c5feee3cad4f5fa1b5a8a968a74d1ddb259295
twistter09 <Ogunladejoshua99@gmail.com>
2024-12-06 16:46:05 +0000

    added portfolio files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Handle form submission for contact
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent successfully!');
+    
+});
+
